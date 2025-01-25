4a69966ad65f19fa56e0671dcc99a10dca0aa358
Joseph Opio <josephopio11@gmail.com>
2025-01-25 10:24:08 +0800

    Task on functions

--- pizza.js
+++ pizza.js
@@ -1,43 +1,51 @@
-var pizzaQuantity
-var pizzaType
-var deliverCity
-var birthDay
-var theText
+var pizzaQuantity;
+var pizzaType;
+var deliverCity;
+var birthDay;
+var theText;
+var pizzaPrice, deliveryPrice, birthdayDiscount;
 
-document.getElementById("numPizzas").addEventListener("change", numChange)
-document.getElementById("typePizza").addEventListener("change", typeChange)
-document.getElementById("deliveryCity").addEventListener("change", cityChange)
-document.getElementById("birthday").addEventListener("change", birthdayChange)
-document.getElementById("placeOrderBtn").addEventListener("click", placeOrder)
-
+document.getElementById("numPizzas").addEventListener("change", numChange);
+document.getElementById("typePizza").addEventListener("change", typeChange);
+document.getElementById("deliveryCity").addEventListener("change", cityChange);
+document.getElementById("birthday").addEventListener("change", birthdayChange);
+document.getElementById("placeOrderBtn").addEventListener("click", placeOrder);
 
 function numChange() {
-    pizzaQuantity = document.getElementById("numPizzas").value
-    console.log("You have ordered " + pizzaQuantity + " pizzas.");
+  pizzaQuantity = document.getElementById("numPizzas").value;
+  console.log("You have ordered " + pizzaQuantity + " pizzas.");
 }
 
-function typeChange(){
-    pizzaType = document.getElementById("typePizza").value;
-    console.log("Your pizza type is " + pizzaType +".");
+function typeChange() {
+  pizzaType = document.getElementById("typePizza").value;
+  console.log("Your pizza type is " + pizzaType + ".");
+  // cheese = 8, pepperoni = 10, supreme =12
+  if (pizzaType === "cheese") {
+    pizzaPrice = 8;
+  } else if (pizzaType === "pepperoni") {
+    pizzaPrice = 10;
+  } else if (pizzaType === "supreme") {
+    pizzaPrice = 12;
+  }
 }
 
-function cityChange(){
-    deliverCity = document.getElementById("deliveryCity").value;
-    console.log("Your delivery city is " + deliverCity +".");
+function cityChange() {
+  deliverCity = document.getElementById("deliveryCity").value;
+  console.log("Your delivery city is " + deliverCity + ".");
+
+  // Anytown = 0, Sacramento = 5, Beverly Hills = 10
 }
 
-function birthdayChange(){
-    birthDay = document.getElementById("birthday").value;
-    if (birthDay === "yes")
-        console.log("You have a birthday discount!")
+function birthdayChange() {
+  birthDay = document.getElementById("birthday").value;
+  if (birthDay === "yes") console.log("You have a birthday discount!");
 
-    if (birthDay === "no")
-        console.log("You don't have a birthday discount.")
+  if (birthDay === "no") console.log("You don't have a birthday discount.");
 
+  // if it is the user's birthday, then give them 10% discount
 }
 
-function placeOrder(){
-    theText = ("Your total will be $" +8*pizzaQuantity+".")
-    document.getElementById("displayTotal").innerHTML = theText
+function placeOrder() {
+  theText = "Your total will be $" + 8 * pizzaQuantity + ".";
+  document.getElementById("displayTotal").innerHTML = theText;
 }
-
